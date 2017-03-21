219e86876f4e7c82b48aa430e839f8ffc95bbeae
Sarah Dutkiewicz <sdutkiewicz@thesoftwareguild.com>
2017-03-21 08:31:27 -0400

    Adding files for the Introduction to JavaScript lesson

---
+++ javascript/1 - Introduction/customJavaScript.js
@@ -0,0 +1,8 @@
+// This is part of the Introduction to Web Development lesson
+var total = 0;
+var firstNumber = 2;
+var secondNumber = 3;
+ 
+total = firstNumber + secondNumber;
+ 
+console.log(total);
