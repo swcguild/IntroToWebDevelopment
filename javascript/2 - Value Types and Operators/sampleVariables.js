edd175e69c6e380a2fdb22ece5c87b183f953c03
Sarah Dutkiewicz <sdutkiewicz@thesoftwareguild.com>
2017-03-30 12:41:48 -0400

    Adding sample for lessons

---
+++ javascript/2 - Value Types and Operators/sampleVariables.js
@@ -0,0 +1,8 @@
+var integerNumberVariable = 42;
+var floatNumberVariable = 3.14;
+var stringVariable = "This is a string.";
+var stringVariable2 = 'This is a string too!';
+var charVariable = 'c';
+var booleanVariable = true;
+var nullVariable = null;
+var undefinedVariable;
