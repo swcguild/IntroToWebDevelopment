ef91a2a6eb7e4491b789731a2b9c124076c46f4a
Sarah Dutkiewicz <sdutkiewicz@thesoftwareguild.com>
2017-03-30 13:40:49 -0400

    Fixing the char reference

--- javascript/2 - Value Types and Operators/sampleVariables.js
+++ javascript/2 - Value Types and Operators/sampleVariables.js
@@ -2,7 +2,7 @@ var integerNumberVariable = 42;
 var floatNumberVariable = 3.14;
 var stringVariable = "This is a string.";
 var stringVariable2 = 'This is a string too!';
-var charVariable = 'c';
+var stringVariable3 = 'c'; // this is a char in other languages
 var booleanVariable = true;
 var nullVariable = null;
 var undefinedVariable;
