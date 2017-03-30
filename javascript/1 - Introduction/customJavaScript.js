87342f42a2967ebe7f0b03d6513a1c4d2bc4e503
Sarah Dutkiewicz <sdutkiewicz@thesoftwareguild.com>
2017-03-30 12:45:35 -0400

    adding comments for our custom JavaScript file

--- javascript/1 - Introduction/customJavaScript.js
+++ javascript/1 - Introduction/customJavaScript.js
@@ -1,4 +1,6 @@
 // This is part of the Introduction to Web Development lesson
+// Learn more at:
+// http://thesoftwareguild.ivyapp.com/javascript
 var total = 0;
 var firstNumber = 2;
 var secondNumber = 3;
