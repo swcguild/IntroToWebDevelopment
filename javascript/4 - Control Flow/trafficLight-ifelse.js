da2739d4a0421a893d87cd52c347ca8dfa895cba
Sarah Dutkiewicz <sdutkiewicz@thesoftwareguild.com>
2017-03-31 10:35:57 -0400

    Samples of if-else...if-else and switch

---
+++ javascript/4 - Control Flow/trafficLight-ifelse.js
@@ -0,0 +1,9 @@
+var trafficLightColor = "green";
+
+if (trafficLightColor == "red"){
+    console.log("STOP");
+} else if (trafficLightColor == "yellow") {
+    console.log("SLOW DOWN");
+} else {
+    console.log("GO!!!!");
+}
---
+++ javascript/4 - Control Flow/trafficLight-switch.js
@@ -0,0 +1,12 @@
+var trafficLightColor = "green";
+
+switch(trafficLightColor){
+    case "red":
+        console.log("STOP");
+        break;
+    case "yellow":
+        console.log("SLOW DOWN");
+        break;
+    default:
+        console.log("GO!!!!");
+}
