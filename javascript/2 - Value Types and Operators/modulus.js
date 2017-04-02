367f97d4baff0bccf278a7b1fe5c28ac7818c892
Sarah Dutkiewicz <sadukie@hotmail.com>
2017-04-01 22:08:39 -0400

    Adding a lesson modulus

---
+++ javascript/2 - Value Types and Operators/modulus.js
@@ -0,0 +1,27 @@
+// The modulus operator (%) returns the remainder of a division problem
+
+// For example, 6 divided by 3 (6/3) = 2 with no remainder
+6 / 3
+// So 6 modulus 3 (6%3) = 0
+6 % 3
+// Now let's try 7 / 3 - notice that the response is a float value
+// This indicates that there should be a remainder
+// This comes back as 2 with a remainder of 1
+7 / 3
+// So 7 modulus 3 (7%3) = 1
+7 % 3
+
+// What can we do with modulus?
+
+// * See if a number is even or odd
+// Any number evenly divisible by 2 is "even".
+
+6 % 2
+// Since there is no remainder, 6 is evenly divisible by 2
+// Therefore, 6 is considered even
+
+
+// * Determine whether a number is a factor of another number
+// If a number is evenly divisible by another number, that number can be considered a factor.
+
+// Since 6 is evenly divisible by 2, then 2 is considered a factor of 6.
